fe14245cdc3c800e6869a92d3a29ecb1f0284a4a
SanaPerween07 <sanahmad15046@gmail.com>
2025-01-28 03:53:19 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: 'localhost', 
+    host: 'https://video-magic-fetcher.onrender.com', 
     port: 8080,
     proxy: {
       '/api': {
