de8e88b05a77ae5aaaa1bd3cb535dd92b01716df
SanaPerween07 <sanahmad15046@gmail.com>
2025-01-28 21:56:25 +0530

    changes

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      '@babel/preset-react',        // To transpile JSX
+      '@babel/preset-typescript'    // Optional: If you're using TypeScript
+    ]
+  };
+  
